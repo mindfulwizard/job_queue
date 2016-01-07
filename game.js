24b67341551d3938c945124d61feb95ed348ba12
Isaac Ratner <isaacratner@gmail.com>
2016-01-07 11:20:47 -0500

    wrapper funcs done

--- game.js
+++ game.js
@@ -1,41 +1,123 @@
-var jq = require('jq');
+var Promise = require('bluebird');
+var jq = require('./wrappers');
 
-var game = Object.create(jq);
+var game = Object.create(null);
 game.id = null;
-game.jobsArray = [];
+game.status = null;
+game.currentTurn = null;
+game.latestJobsArray = null;
+game.workingMachinesArray = [];
 
-game.startGame = function() {
-	game.createGame()
-	.then(function(res) {
-		game.id = res.id;
-	});
-};
-
-game.nextTurn = function() {
-	game.advanceTurn(game.id)
-	.then(function(res) {
-		game.jobsArray = res.jobs;
+game.createNewMachine = function() {
+	jq.newMachine(game.id)
+	.then(function(newMachine) {
+		console.log('newMachine response', newMachine)
+		game.workingMachinesArray.push(newMachine);
+		game.allocate();
 	})
+}
+
+game.advanceTurn = function() {
+	jq.advanceTurn(game.id)
+	.then(function(turnInfo) {
+		console.log('advanceTurn response', turnInfo)
+		game.status = turnInfo.status;
+		game.latestJobsArray = turnInfo.jobs;
+		game.currentTurn = turnInfo.current_turn;
+	})
+}
+
+game.checkStatus = function() {
+	var machineStatusArray = [];
+
+	for(var i = 0; i < game.workingMachinesArray.length; i++) {
+		var machine = game.workingMachinesArray[i];
+
+		if(!machine.jobs) {
+			console.log('YO');
+			machineStatusArray.push({id: machine.id, availableMemory: 64});
+			break;
+		}
+
+		for(var j = 0; j < machine.jobs.length; j++) {
+			var mem = 0;
+			if(game.currentTurn <= machine.jobs[j].turns) {
+				mem += machine.jobs[j].memoryUsed;
+				machineStatusArray.push({id: machine.id, availableMemory: 64 - mem});
+			}
+		}
+	}
+	return machineStatusArray;
+}
+
+game.allocate = function() {
+	//need to delete machines
+	var machineStatusArray = game.checkStatus();
+	if(!machineStatusArray) {
+		game.createNewMachine();
+	}
+
+	for(var i = 0; i < machineStatusArray.length; i++) {
+		var jobs = [];
+		jobsList = [];
+		var machine = machineStatusArray[i];
+		for(var j = 0; j < game.latestJobsArray.length; j++) {
+			if(machine.availableMemory >= game.latestJobsArray[j].memory_required) {
+				var currentJob = game.latestJobsArray[j];
+				jobs.push({id: currentJob.id, memoryUsed: currentJob.memory_required, turns: currentJob.turns_required});
+				jobsList.push(currentJob.id);
+				machine.availableMemory = machine.availableMemory - game.latestJobsArray[j].memory_required;
+			}
+			break;
+		}
+		jq.assign(game.id, machine.id, jobsList)
+		.then(function(res) {
+			console.log('assign response', res)
+		})
+		//update workingMachinesArray
+		for(var k = 0; k < game.workingMachinesArray.length; k++) {
+			if(game.workingMachinesArray[k].id === machine.id) {
+				game.workingMachinesArray[k].jobs = jobs;
+			}	
+		}
+	}
 };
 
-game.addMachine = function() {
-	game.newMachine(game.id);
+game.run = function() {
+	game.advanceTurn()
+	if(game.status !== 'completed') {
+		game.allocate()
+		game.run()
+  	} else {
+	  	console.log('Winner')
+	  	game.getInfo(game.id)
+	  	.then(function(res) {
+	  		console.log('winning info', res)
+	  	})  		
+  	}
+}
+
+game.start = function() {
+	jq.createGame()
+	.then(function(gameState) {
+		console.log('createGame response', gameState)
+		game.id = gameState.id;
+		game.status = gameState.status;
+		game.run();
+	// 	return jq.advanceTurn(game.id);
+	// })
+	// .then(function(turnInfo) {
+	// 	console.log('advanceTurn response', turnInfo)
+	// 	game.latestJobsArray = turnInfo.jobs;
+	// 	game.currentTurn = turnInfo.current_turn;
+	// 	return jq.newMachine(game.id);
+	// })
+	// .then(function(newMachine) {
+	// 	console.log('newMachine response', newMachine)
+	// 	game.workingMachinesArray.push(newMachine);
+	// 	game.allocate();
+	// })
 };
 
-game.deleteMachine = function(machineId) {
-	game.terminateMachine(game.id, machineId);
-};
-
-game.assignJobs = function(machineId, jobs) {
-	game.assign(game.id, machineId, jobs);
-};
-
-game.play = function() {
-
-};
-
-game.play();
-
-
-// watch memory, if jobs are queued spin up new machine when 64gb limit approaching
-// if any delay, spin up new machine
+game.start();
+//game.terminateMachine(game.id, machineId);
--- wrappers.js
+++ wrappers.js
@@ -1,61 +1,103 @@
+var Promise = require('bluebird');
 var request = require('request');
 var host = 'http://job-queue-dev.elasticbeanstalk.com';
-module.exports = jq;
 
 var jq = Object.create(null);
+module.exports = jq;
 
 jq.createGame = function() {
-	request.post(host + '/games', {form: {}},
-		function(error, response, body) {
-			if(!error) {
-				return JSON.parse(body);
-			} else {
-				return 'Could not create game.'
+	return new Promise(function(resolve, reject) {
+		request.post(host + '/games', {form: {}}, function(error, response, body) {
+			if(error) {
+				reject(error);
+				return;
 			}
-		}
-	);
-};
-
-jq.getInfo = function(gameId) {
-	request.get(host + '/games/' + gameId,
-		function(error, response, body) {
-			return JSON.parse(body);
-		}
-	);
+			try {
+				resolve(JSON.parse(body));
+			} catch(e) {
+				reject('invalid JSON response received');
+			}
+		});	
+	})
 };
 
 jq.advanceTurn = function(gameId) {
-	request.get(host + '/games/' + gameId + '/next_turn',
-		function(error, response, body) {
-			return JSON.parse(body);
-		}
-	);
+	return new Promise(function(resolve, reject) {
+		request.get(host + '/games/' + gameId + '/next_turn', function(error, response, body) {
+			if(error) {
+				reject(error);
+				return;
+			}
+			try {
+				resolve(JSON.parse(body));
+			} catch(e) {
+				reject('Problem advancing turn');
+			}
+		});
+	})
 };
 
 jq.newMachine = function(gameId) {
-	request.post(host + '/games/' + gameId + '/machines', {form: {}},
-		function(error, response, body) {
-			return JSON.parse(body);
-		}
-	);
-};
-
-jq.terminateMachine = function(gameId, machineId) {
-	request.delete(host + '/games/' + gameId + '/machines/' + machineId, {form: {}},
-		function(error, response, body) {
-			return JSON.parse(body);
-		}
-	);
+	return new Promise(function(resolve, reject) {
+		request.post(host + '/games/' + gameId + '/machines', function(error, response, body) {
+			if(error) {
+				reject(error);
+				return;
+			}
+			try {
+				resolve(JSON.parse(body));
+			} catch(e) {
+				reject('Problem creating new machine');
+			}
+		});	
+	})
 };
 
 jq.assign = function(gameId, machineId, jobIds) {
-	request.post(host + '/games/' + gameId + '/machines/' + machineId + '/job_assignments', {{ job_ids: JSON.stringify(jobIds) }},
-		function(error, response, body) {
-			return JSON.parse(body);
-		}
-	);	
+	return new Promise(function(resolve, reject) {
+		request.post(host + '/games/' + gameId + '/machines/' + machineId + '/job_assignments', {form: {job_ids: JSON.stringify(jobIds)}}, function(error, response, body) {
+			if(error) {
+				reject(error);
+				return;
+			}
+			try {
+				resolve(JSON.parse(body));
+			} catch(e) {
+				reject('Problem assigning machine');
+			}
+		});	
+	})
 };
 
+jq.terminateMachine = function(gameId, machineId) {
+	return new Promise(function(resolve, reject) {
+		request.delete(host + '/games/' + gameId + '/machines/' + machineId, {form: {}}, function(error, response, body) {
+			if(error) {
+				reject(error);
+				return;
+			}
+			try {
+				resolve(JSON.parse(body));
+			} catch(e) {
+				reject('Problem terminating machine');
+			}
+		});	
+	})
+};
 
-
+jq.getInfo = function(gameId) {
+	return new Promise(function(resolve, reject) {
+		request.get(host + '/games/' + gameId, function(error, response, body) {
+			if(error) {
+				reject(error);
+				return;
+			}
+			try {
+				resolve(JSON.parse(body));
+			} catch(e) {
+				reject('Problem retrieving info');
+			}
+		});	
+	})
+};
 
